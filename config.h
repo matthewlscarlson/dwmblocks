53448905e05527de9a801c22c5ae26ba97ee9aee
Matthew Carlson <matt@mcarlson.xyz>
2021-12-02 18:09:15 -0500

    rss back

--- config.h
+++ config.h
@@ -20,9 +20,10 @@ static Block blocks[] = {
     {"music.sh",   1, 1},
     {"pkgs.sh",    1, 2},
     {"mail.sh",    1, 3},
-    {"net.sh",     1, 4},
-    {"vol.sh",     1, 5},
-    {"light.sh",   1, 6},
-    {"bat.sh",     1, 7},
-    {"time.sh",    1, 8},
-    {"date.sh",    1, 9}};
+    {"rss.sh",     1, 4},
+    {"net.sh",     1, 5},
+    {"vol.sh",     1, 6},
+    {"light.sh",   1, 7},
+    {"bat.sh",     1, 8},
+    {"time.sh",    1, 9},
+    {"date.sh",    1, 10}};
