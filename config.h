688dfa6546544947c6508b0aeb015c45ddc627d6
Matthew Carlson <matt@mcarlson.xyz>
2022-01-30 12:25:58 -0500

    changes

--- config.h
+++ config.h
@@ -20,10 +20,9 @@ static Block blocks[] = {
     {"music.sh",   1, 1},
     {"pkgs.sh",    1, 2},
     {"mail.sh",    1, 3},
-    {"rss.sh",     1, 4},
-    {"net.sh",     1, 5},
-    {"vol.sh",     1, 6},
-    {"light.sh",   1, 7},
-    {"bat.sh",     1, 8},
-    {"time.sh",    1, 9},
-    {"date.sh",    1, 10}};
+    {"net.sh",     1, 4},
+    {"vol.sh",     1, 5},
+    {"light.sh",   1, 6},
+    {"bat.sh",     1, 7},
+    {"time.sh",    1, 8},
+    {"date.sh",    1, 9}};
