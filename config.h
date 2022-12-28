bd7532d93c972e8de28af290ef27bb5d55d42814
Matthew Carlson <matt@mattcarlson.org>
2022-12-28 03:34:06 -0500

    rearranged modules

--- config.h
+++ config.h
@@ -19,11 +19,10 @@ typedef struct {
 static Block blocks[] = {
     {"pkgs.sh",    1, 1},
     {"mail.sh",    1, 2},
-    {"rss.sh",     1, 3},
-    {"torr.sh",    1, 4},
-    {"net.sh",     1, 5},
-    {"vol.sh",     1, 6},
-    {"light.sh",   1, 7},
-    {"bat.sh",     1, 8},
-    {"time.sh",    1, 9},
-    {"date.sh",    1, 10}};
+    {"torr.sh",    1, 3},
+    {"net.sh",     1, 4},
+    {"vol.sh",     1, 5},
+    {"light.sh",   1, 6},
+    {"bat.sh",     1, 7},
+    {"time.sh",    1, 8},
+    {"date.sh",    1, 9}};
